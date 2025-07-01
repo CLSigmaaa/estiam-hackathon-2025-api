60968cca9a179ae980064bc52c7ae21476fb00c9
VeXRod <max.bohn57@gmail.com>
2025-07-01 11:31:37 +0200

    Init commit

---
+++ src/main/java/com/estiam/monitorcontrol/MonitorcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.estiam.monitorcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estiam/monitorcontrol/MonitorcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estiam.monitorcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
